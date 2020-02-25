a6777eedae9ad59e4703a1a20573ef18152cd81c
Sama Gyaneshwar <sama.gyaneshwar@gmail.com>
2020-02-25 07:43:35 +0530

    [refactor] added firebase storage URL

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL : process.env.REACT_APP_FB_STORAGE
+    baseURL : 'https://react-burger-fbed8.firebaseio.com/'
 });
 
 export default instance;
